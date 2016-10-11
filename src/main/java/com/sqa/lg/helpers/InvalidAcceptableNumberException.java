7cd9f8a382c0911e0b65932748977cbccc7602af
Greenster <Baptisimo@yahoo.com>
2016-10-10 21:36:26 -0400

    Create RequestInput Helper Class

---
+++ src/main/java/com/sqa/lg/helpers/InvalidAcceptableNumberException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: InvalidAcceptableNumberException.java<br>
+ *
+ *   Green, Lorne<br>
+ *   Java <br>
+ *   <br>
+ *   Created: Oct 10, 2016
+ *   
+ */
+
+package com.sqa.lg.helpers;
+
+/**
+ * InvalidAcceptableNumberException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Green, Lorne
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class InvalidAcceptableNumberException extends Exception {
+
+}
---
+++ src/main/java/com/sqa/lg/helpers/InvalidAcceptableWordException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: InvalidAcceptableWordException.java<br>
+ *
+ *   Green, Lorne<br>
+ *   Java <br>
+ *   <br>
+ *   Created: Oct 10, 2016
+ *   
+ */
+
+package com.sqa.lg.helpers;
+
+/**
+ * InvalidAcceptableWordException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Green, Lorne
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class InvalidAcceptableWordException extends Exception {
+
+}
